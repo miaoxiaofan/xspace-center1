cec5c64f93bb44585349804b3dbe82cee6cd089d
wmiao <1429425727@qq.com>
2019-08-25 21:19:56 +0800

    kafka

--- order-center/order-center-service/src/main/java/com/xspace/ordercenter/kafka/KafkaProducer.java
+++ order-center/order-center-service/src/main/java/com/xspace/ordercenter/kafka/KafkaProducer.java
@@ -2,10 +2,12 @@ package com.xspace.ordercenter.kafka;
 
 import com.alibaba.fastjson.JSON;
 import com.xspace.ordercenter.entity.Employee;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Component;
 import org.springframework.util.concurrent.ListenableFuture;
 
@@ -28,7 +30,14 @@ public class KafkaProducer {
             employee.setUserName("李四");
             employee.setPhoneNum("15751003699");
         }
-        logger.info("向  topic:{} 推送数据，内容：{}","atopic",JSON.toJSONString(employee));
-        ListenableFuture send = kafkaTemplate.send("atopic", "key", JSON.toJSONString(employee));
+        logger.info("向  topic:{} 推送数据，内容：{}", "atopic", JSON.toJSONString(employee));
+        ListenableFuture<SendResult<String,String>> future = kafkaTemplate.send("atopic", "key", JSON.toJSONString(employee));
+
+        try {
+            RecordMetadata recordMetadata= future.get().getRecordMetadata();
+            logger.info("result:{}",recordMetadata);
+        } catch (Exception e) {
+            logger.error("error"+e);
+        }
     }
 }
