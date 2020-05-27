5ae2d1e35130596f70c1b85ed88659f3de3bee7e
miaow1 <miaow1@chinaunicom.cn>
2020-05-27 15:23:57 +0800

    nio demo

---
+++ common-center/src/main/java/com/xspace/commoncenter/nio/ByteBufferDemo.java
@@ -0,0 +1,39 @@
+package com.xspace.commoncenter.nio;
+
+import java.nio.ByteBuffer;
+
+/**
+ * @author wmiao
+ * @description
+ * @date 2020/5/25
+ */
+
+public class ByteBufferDemo {
+
+
+
+
+    public static void main(String[] args) {
+
+        // 创建一个缓冲区
+        ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+
+        // 看一下初始时4个核心变量的值
+        System.out.println("初始时-->limit--->"+byteBuffer.limit());
+        System.out.println("初始时-->position--->"+byteBuffer.position());
+        System.out.println("初始时-->capacity--->"+byteBuffer.capacity());
+        System.out.println("初始时-->mark--->" + byteBuffer.mark());
+
+        System.out.println("--------------------------------------");
+
+        // 添加一些数据到缓冲区中
+        String s = "Java3y";
+        byteBuffer.put(s.getBytes());
+
+        // 看一下初始时4个核心变量的值
+        System.out.println("put完之后-->limit--->"+byteBuffer.limit());
+        System.out.println("put完之后-->position--->"+byteBuffer.position());
+        System.out.println("put完之后-->capacity--->"+byteBuffer.capacity());
+        System.out.println("put完之后-->mark--->" + byteBuffer.mark());
+    }
+}
