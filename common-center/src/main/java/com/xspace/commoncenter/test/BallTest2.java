1c37a1d277834274c8295509307faa0bfe40bdb5
miaow1 <miaow1@chinaunicom.cn>
2020-06-04 10:29:12 +0800

    nio noblockdemo

--- common-center/src/main/java/com/xspace/commoncenter/test/BallTest2.java
+++ common-center/src/main/java/com/xspace/commoncenter/test/BallTest2.java
@@ -14,7 +14,7 @@ public class BallTest2 {
     public static void main(String[] args) {
 
         for (int i = 0; i < 5; i++) {
-            System.out.println(test());
+            System.out.println("第"+(i+1)+"次："+test());
         }
 
     }
